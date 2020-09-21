3727c0b7c247adc73b482125ca94305604528224
gsGupta11 <gaurishankar.gupta2018@vitstudent.ac.in>
2020-09-21 19:57:21 +0530

    Update: Basic Project Setup

--- pythonScripts/Image_to_text.py
+++ pythonScripts/Image_to_text.py
@@ -8,11 +8,11 @@ Created on Sun Sep 13 13:18:23 2020
 import pytesseract as tess
 from PIL import Image
 
-tess.pytesseract.tesseract_cmd=r'C:\Users\kingslayer\AppData\Local\Tesseract-OCR\tesseract.exe'
+# tess.pytesseract.tesseract_cmd=r'C:\Users\kingslayer\AppData\Local\Tesseract-OCR\tesseract.exe'
 
 def convertImageToText(img_address):
     img=Image.open(img_address)
     text=tess.image_to_string(img)
     return text
 
-convertImageToText('test_ImageToText.png')
+print(convertImageToText('test_ImageToText.png'))
