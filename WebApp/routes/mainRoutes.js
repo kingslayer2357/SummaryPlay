a6ffe3aa566e95ad3c0a57ad7121653faec8f286
gsGupta11 <gaurishankar.gupta2018@vitstudent.ac.in>
2020-10-08 04:26:51 +0530

    TODO: Some Weird stylesheet Problem

--- WebApp/routes/mainRoutes.js
+++ WebApp/routes/mainRoutes.js
@@ -3,7 +3,14 @@ const router = express.Router();
 let ejs = require('ejs');
 
 router.get("/",async (req,res,next)=>{
-    res.render("../views/home");
+    res.render("home");
+})
+router.get("/text", async (req,res,next)=>{
+    res.render("textinput");
+})
+
+router.get("/textuploader", async (req,res,next)=>{
+    res.render("uploader");
 })
 
 module.exports = router;
